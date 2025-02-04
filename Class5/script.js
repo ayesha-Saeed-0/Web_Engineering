a756a6c2dea12f3b1f7c242fc37e286ef7a61b88
ayesha-Saeed-0 <qemerald1@gmail.com>
2025-02-04 12:42:16 +0500

    High Order Functions

--- Class5/script.js
+++ Class5/script.js
@@ -2,8 +2,8 @@
 //     console.log("Hello");
 // }
 
-//----------------------------------------------------------------------------------------
-//Displaying Duplicates
+// // ----------------------------------------------------------------------------------------
+// // Displaying Duplicates
 
 // let greet = function (name) {
 //   return `Welcome ${name}`;
@@ -24,22 +24,42 @@
 // }
 
 // findDuplicates([1, 2, 3, 4, 2, 5, 6, 3, 7, 8, 1]);
-//-----------------------------------------------------------------------------------------------
+// // -----------------------------------------------------------------------------------------------
 
-// Arrow Function
+// // Arrow Function
 
-let names = (name) => {
-  //same as below
-  return "Hello" + name;
+// let names = (name) => {
+//   //same as below
+//   return "Hello" + name;
+// };
+
+// let nam = (name) => "Hello" + name; //same as below
+
+// const displayDuplicates = (arr, element) => ({
+//   allDuplicates: [...new Set(arr.filter((item, i) => arr.indexOf(item) !== i))],
+//   isDuplicate: arr.filter((item) => item === element).length > 1,
+// });
+
+// console.log(displayDuplicates([1, 2, 3, 2, 4, 5, 3, 6], 2));
+
+// console.log(displayDuplicates([1, 2, 3, 4, 5], 6));
+// //--------------------------------------------------------------------------------------------
+
+// High Order Functions
+//Not High Order Function
+let add = function (a, b) {
+  return a + b;
 };
 
-let nam = (name) => "Hello" + name; //same as below
+let multiply = function (a, b) {
+  return a * b;
+};
+let manipulator = function (a, b, func) {
+  return func(a, b);
+};
 
-const displayDuplicates = (arr, element) => ({
-  allDuplicates: [...new Set(arr.filter((item, i) => arr.indexOf(item) !== i))],
-  isDuplicate: arr.filter((item) => item === element).length > 1,
-});
+const add = (a, b) => a + b; //High Order Function
+const multiply = (a, b) => a * b;
+const manipulator = (a, b, func) => func(a, b);
 
-console.log(displayDuplicates([1, 2, 3, 2, 4, 5, 3, 6], 2));
-
-console.log(displayDuplicates([1, 2, 3, 4, 5], 6));
+console.log(manipulator(3, 5, multiply));
